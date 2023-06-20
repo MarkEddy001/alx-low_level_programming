47745bbc5afbea313d777eddbeff7137129d40fb
MarkEddy001 <markeddy254@gmail.com>
2023-06-20 02:35:07 -0700

    This repository contains the tasks for 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unstd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set approprietely.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1))
+}
