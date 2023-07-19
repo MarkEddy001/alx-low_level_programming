7d47b3f789098a2fdb07e26c717b39c57bc75e2e
Wanyoike Edwards <57659295+MarkEddy001@users.noreply.github.com>
2023-07-19 14:01:46 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,60 +1,56 @@
 #include "3-calc.h"
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
 
 /**
-* op_add - returns sum of a and b
-* @a: first num
-* @b: second num
-* Return: sum of a and b
-*/
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
-* op_sub - subs a and b
-* @a: first num
-* @b: second num
-* Return: difference of a and b
-*/
+ * op_sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of a and b
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
-* op_mul - muls a and b
-* @a: first num
-* @b: second num
-* Return: product of a and b
-*/
+ * op_mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of a and b
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
-* op_div - divides a and b
-* @a: first num
-* @b: second num
-* Return: result of division of a and b
-*/
+ * op_div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the division of a by b
+ */
 int op_div(int a, int b)
 {
+	/* todo: handle division by 0 */
 	return (a / b);
 }
 
 /**
-* op_mod - returns remainder of division of a by b
-* @a: first num
-* @b: second num
-* Return: remainder of division of a by b
-*/
+ * op_mod - computes the remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of the division of a by b
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
