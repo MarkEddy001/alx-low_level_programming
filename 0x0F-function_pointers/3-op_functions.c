e4249e568cad5c25fc83c353eb88b63e9001b5e5
MarkEddy001 <markeddy254@gmail.com>
2023-07-19 03:11:50 -0700

    Files containing the 5 follwing functions op_add: returns the sum of a and b. Prototype: int op_add(int a, int b); op_sub: returns the difference of a and b. Prototype: int op_sub(int a, int b); op_mul: returns the product of a and b. Prototype: int op_mul(int a, int b); op_div: returns the result of the division of a by b. Prototype: int op_div(int a, int b); op_mod: returns the remainder of the division of a by b. Prototype: int op_mod(int a, int b);

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,6 +1,9 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 /**
 * op_add - returns sum of a and b
