c01762d1a02097fd9cd185a862428e4d246ddfe1
Wanyoike Edwards <57659295+MarkEddy001@users.noreply.github.com>
2023-10-16 13:11:34 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/MarkEddy001/0x18.c/raw/master/gm
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp https://github.com/MarkEddy001/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
+export LD_PRELOAD=/tmp/hacked.so
